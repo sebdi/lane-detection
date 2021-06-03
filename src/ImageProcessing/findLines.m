bf585f30ddb64482c165e27dcc37c4f1b8664cf5
Sebastian Dingler <s.dingler@googlemail.com>
2021-06-03 20:22:43 +0200

    vision is not supported by newer MATLAB versions anymore. Replaced functions with similar one. Tested with MATLAB 2020a

--- src/ImageProcessing/findLines.m
+++ src/ImageProcessing/findLines.m
@@ -5,17 +5,8 @@ function [ BW ] = findLines( I )
 %
 % LICENSE github.com/sebdi/lane_detection/blob/master/LICENSE
 %
-% DATE    29.08.2016
-hedge = vision.EdgeDetector;
-hhoughtrans = vision.HoughTransform(pi/360,'ThetaRhoOutputPort', true);
-hfindmax = vision.LocalMaximaFinder(10,	'HoughMatrixInput', true);
-hfindmax.NeighborhoodSize = [255 177];
-hLocalMax.Threshold = 200;
-hhoughlines = vision.HoughLines('SineComputation','Trigonometric function');
+% DATE    03.06.2021
 
-% Find the edges in the intensity image
-I2 = I;
-BW = step(hedge, I2);
-
+BW = edge(I);
 end
 
