a07f8e07cf0936141b52e8746cde12fe186ad1bb
Sebastian Dingler <s.dingler@googlemail.com>
2022-12-04 22:45:30 +0100

    Renamed variables r and c to row and col for better readability.

--- src/getMeasurement.m
+++ src/getMeasurement.m
@@ -6,8 +6,8 @@ function measurement = getMeasurement(lane)
 % LICENSE github.com/sebdi/lane_detection/blob/master/LICENSE
 %
 % DATE    01.09.2016
-x1 = linspace(min(lane.r),max(lane.r));
-f1 = polyval(lane.p,x1);
+x1 = linspace(min(lane.r),max(lane.r)); %lane.r contains the row values
+f1 = polyval(lane.p,x1); %lane.p contains the coefficients of the polynomial
 % a_2, a_1, a_0, x_start, y_start, x_ende, y_ende
 measurement = [lane.p';f1(1);min(lane.r);f1(end);max(lane.r)];
 end
