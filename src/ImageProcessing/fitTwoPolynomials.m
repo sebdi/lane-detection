1cd9d59d928a9970b9105946cff57d7ca6651bbc
Sebastian Dingler <s.dingler@googlemail.com>
2022-12-11 17:04:27 +0100

    Renamed variables r and c to row and col for better readability.

--- src/ImageProcessing/fitTwoPolynomials.m
+++ src/ImageProcessing/fitTwoPolynomials.m
@@ -1,4 +1,4 @@
-function [p_1, p_2, r_1, r_2, error_1, error_2, succ] = fitTwoPolynomials(r,c)
+function [p_1, p_2, r_1, r_2, error_1, error_2, succ] = fitTwoPolynomials(row,col)
 %FITTWOPOLYNOMIALS Fits 2 concatenated polynomials to a set of points
 %
 % AUTHOR  Sebastian Dingler <s.dingler@gmail.com>
@@ -16,14 +16,14 @@ S_2 = [];
 succ = 0;
 error_1 = 0;
 error_2 = 0;
-idx = 20:4:(size(r,1)-20);
+idx = 20:4:(size(row,1)-20);
 for l=1:length(idx)
-    if (max(r(1:idx(l)))-min(r(1:idx(l))))>80 && (max(r(idx(l)+1:end))-min(r(idx(l)+1:end)))>80
-        r_1{n} = r(1:idx(l));
-        r_2{n} = r(idx(l)+1:end);
-        [p_1{n},S] = polyfit(r(1:idx(l)),c(1:idx(l)),2);
+    if (max(row(1:idx(l)))-min(row(1:idx(l))))>80 && (max(row(idx(l)+1:end))-min(row(idx(l)+1:end)))>80
+        r_1{n} = row(1:idx(l));
+        r_2{n} = row(idx(l)+1:end);
+        [p_1{n},S] = polyfit(row(1:idx(l)),col(1:idx(l)),2);
         error_1(n) = S.normr;
-        [p_2{n},S] = polyfit(r(idx(l)+1:end),c(idx(l)+1:end),2);
+        [p_2{n},S] = polyfit(row(idx(l)+1:end),col(idx(l)+1:end),2);
         error_2(n) = S.normr;
         n = n + 1;
         succ = 1;
